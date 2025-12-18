1b51ddbe533293e450b4f899fcaf23f292493218
Qusay_Dwekat <qsay.3w@gmail.com>
2025-12-18 17:09:05 +0200

    refactor: Rename testRoutes file and restore its content; ensure consistent casing in file paths

--- Backend/src/routes/TestRoutes/testRoutes.js
+++ Backend/src/routes/TestRoutes/testRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { generateQuestions, saveTestResult } = require('../controllers/TestController/testController');
-const { protect } = require('../middleware/authMiddleware');
+const { generateQuestions, saveTestResult } = require('../../controllers/TestController/testController');
+const { protect } = require('../../middleware/authMiddleware');
 
 router.post('/generate-questions', generateQuestions);
 router.post('/results', protect, saveTestResult);
